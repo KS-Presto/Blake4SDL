5f17b2e4746ef1d8924d3f936ffa5640c79b5ed4
KillerSoftBP <emelya177@gmail.com>
2024-03-20 22:25:19 +0000

    Cleanup jm_io
    Use C library file pointers
    
    We don't do far reads/writes anymore
    
    I'm not even sure if Blake uses LZH compression anywhere? But I guess we'll find out later...

--- jm_io.c
+++ jm_io.c
@@ -1,216 +1,142 @@
-#include "id_heads.h"
+// JM_IO.C
 
-#include <io.h>
+#include "3d_def.h"
 
-#include "jm_io.h"
+#ifdef NOTYET
 #include "jm_cio.h"
 #include "jm_lzh.h"
-#include "jm_error.h"
-
-//--------------------------------------------------------------------------
-// IO_FarRead()
-//--------------------------------------------------------------------------
-boolean IO_FarRead (int handle, byte far *dest, long length)
-{
-	unsigned readlen,nread;
-
-	while (length)
-	{
-		if (length > 0xffff)
-			readlen=0xffff;
-		else
-			readlen=length;
-
-		_dos_read(handle,dest,readlen,&nread);
-		if (nread != readlen)
-			return(false);
-
-		length -= readlen;
-	}
-
-	return(true);
-}
-
-//--------------------------------------------------------------------------
-// IO_FarWrite()
-//--------------------------------------------------------------------------
-boolean IO_FarWrite (int handle, byte far *source, long length)
-{
-	unsigned writelen,nwritten;
-
-	while (length)
-	{
-		if (length > 0xffff)
-			writelen=0xffff;
-		else
-			writelen=length;
-
-		_dos_write(handle,source,writelen,&nwritten);
-		if (nwritten != writelen)
-			return(false);
-
-		length -= writelen;
-	}
-
-	return(true);
-}
+#endif
 
 #if DEMOS_EXTERN
 
-//--------------------------------------------------------------------------
-// IO_WriteFile()
-//--------------------------------------------------------------------------
-boolean IO_WriteFile(char *filename, void far *ptr, long length)
+/*
+===================
+=
+= IO_WriteFile
+=
+===================
+*/
+
+void IO_WriteFile (char *filename, void *source, int32_t length)
 {
-	int handle;
-	long size;
+    FILE *file;
 
-	handle = open(filename,O_CREAT | O_BINARY | O_WRONLY,
-				S_IREAD | S_IWRITE | S_IFREG);
+    file = fopen(filename,"wb");
 
-	if (handle == -1)
-		return(false);
+    if (!file)
+        CA_CannotOpen (filename);
 
-	if (!IO_FarWrite (handle,ptr,length))
-	{
-		close (handle);
-		return(false);
-	}
-	close (handle);
-	return(true);
-}
+    if (!fwrite(source,length,1,file))
+        Quit ("Error writing file %s: %s",filename,strerror(errno));
 
-#endif				 
-
-//--------------------------------------------------------------------------
-// IO_LoadFile()
-//--------------------------------------------------------------------------
-long IO_LoadFile (char *filename, memptr *dst)
-{
-	char buffer[5]={0,0,0,0,0};
-	int handle;
-	long size=0;
-
-	if ((handle = open(filename,O_RDONLY | O_BINARY, S_IREAD)) == -1)
-		return(size);
-
-	read(handle,buffer,4);
-	if (!strcmp(buffer,"JAMP"))
-	{
-		struct JAMPHeader head;
-
-		read(handle,&head,sizeof(head));
-		size = head.OriginalLen;
-		switch (head.CompType)
-		{
-			case ct_LZH:
-				LZH_Startup();
-				MM_GetPtr(dst,head.OriginalLen);
-				if (mmerror)
-					return(0);
-				LZH_Decompress((void far *)handle,*dst,size,head.CompressLen,SRC_FILE|DEST_MEM);
-				LZH_Shutdown();
-			break;
-
-			case ct_LZW:
-				IO_ERROR(IO_LOADFILE_NO_LZW);
-			break;
-
-			default:
-				IO_ERROR(IO_LOADFILE_UNKNOWN);
-			break;
-		}
-	}
-	else
-	{
-		lseek(handle,0,SEEK_SET);
-		size = filelength(handle);
-		MM_GetPtr(dst,size);
-		if (!IO_FarRead(handle,*dst,size))
-		{
-			close(handle);
-			return(size);
-		}
-	}
-
-	close(handle);
-
-	return(size);
-}
-
-#if 0
-
-//--------------------------------------------------------------------------
-// IO_CopyFile()
-//--------------------------------------------------------------------------
-void IO_CopyFile(char *sFilename, char *dFilename)
-{
-	int sHandle,dHandle;
-	unsigned length;
-
-// Allocate memory for buffer.
-//
-	if ((sHandle = open(sFilename,O_RDONLY | O_BINARY, S_IREAD)) == -1)
-		IO_ERROR(IO_COPYFILE_OPEN_SRC);
-
-	if ((dHandle=open(dFilename,O_CREAT|O_RDWR|O_BINARY,S_IREAD|S_IWRITE))==-1)
-		IO_ERROR(IO_COPYFILE_OPEN_DEST);
-
-// Copy that file!
-//
-	IO_CopyHandle(sHandle,dHandle,-1);
-
-// Close files.
-//
-	close(sHandle);
-	close(dHandle);
+    fclose (file);
 }
 
 #endif
 
-//--------------------------------------------------------------------------
-// IO_CopyHandle()
-//--------------------------------------------------------------------------
-void IO_CopyHandle(int sHandle, int dHandle, long num_bytes)
+/*
+===================
+=
+= IO_LoadFile
+=
+= Don't forget to free *dest!
+=
+===================
+*/
+
+int32_t IO_LoadFile (char *filename, void **dest)
 {
-	extern boolean bombonerror;
+#ifdef NOTYET
+    JAMPHeader head;
+#endif
+    FILE       *file;
+    const char *id = "JAMP";
+    char       *buffer;
+    int        len;
+    int32_t    size;
 
-	#define CF_BUFFER_SIZE 8192
+    file = fopen(filename,"rb");
 
-	long fsize;
-	memptr src;
+    if (!file)
+        CA_CannotOpen (filename);
 
-	unsigned length;
+    len = strlen(id) + 1;
+    buffer = SafeMalloc(len);
 
-// Allocate memory for buffer.
-//
-	MM_GetPtr(&src,CF_BUFFER_SIZE);
-	if (num_bytes == -1)
-		fsize=filelength(sHandle);
-	else
-		fsize=num_bytes;
+    fread (buffer,len - 1,1,file);
 
-// Copy that file!
-//
-	do
-	{
-	// Determine length to read/write.
-	//
-		if (fsize >= CF_BUFFER_SIZE)
-			length = CF_BUFFER_SIZE;
-		else
-			length = fsize;
+    if (!strcmp(buffer,id))
+    {
+#ifdef NOTYET
+        fread (&head,sizeof(head),1,file);
+        size = head.OriginalLen;
 
-	// Read it, write it and decrement length.
-	//
-		IO_FarRead(sHandle,src,length);
-		IO_FarWrite(dHandle,src,length);
-		fsize -= length;
-	}
-	while (fsize);
+        switch (head.CompType)
+        {
+            case ct_LZH:
 
-// Free buffer.
-//
-	MM_FreePtr(&src);
+                LZH_Startup ();
+
+                *dest = SafeMalloc(head.OriginalLen);
+
+                LZH_Decompress ((void far *)handle,*dest,size,head.CompressLen,SRC_FILE | DEST_MEM);
+                LZH_Shutdown();
+                break;
+
+            case ct_LZW:
+                Quit ("No code for LZW compression!");
+                break;
+
+            default:
+                Quit ("Unknown compression type!");
+                break;
+        }
+#else
+        Quit ("Compression not yet supported!");
+#endif
+    }
+    else
+    {
+        size = CA_GetFileLength(file);
+        *dest = SafeMalloc(size);
+
+        if (!fread(*dest,size,1,file))
+            Quit ("Error reading file %s: %s",filename,strerror(errno));
+    }
+
+    fclose (file);
+
+    free (buffer);
+
+    return size;
 }
 
+
+/*
+===================
+=
+= IO_CopyFile
+=
+===================
+*/
+
+void IO_CopyFile (FILE *sourcefile, FILE *destfile, int32_t numbytes)
+{
+    void   *src;
+    size_t fsize;
+
+    if (numbytes == -1)
+        fsize = CA_GetFileLength(sourcefile);
+    else
+        fsize = numbytes;
+
+    src = SafeMalloc(fsize);
+
+    if (fread(src,fsize,1,sourcefile))
+        Quit ("Error reading source file: %s",strerror(errno));
+
+    if (fwrite(src,fsize,1,destfile))
+        Quit ("Error writing destination file: %s",strerror(errno));
+
+    free (src);
+}
--- jm_io.h
+++ jm_io.h
@@ -1,15 +1,12 @@
+// JM_IO.H
+
 #ifndef _JM_IO_H_
 #define _JM_IO_H_
 
-//-------------------------------------------------------------------------
-// Function Prototypes
-//-------------------------------------------------------------------------
-boolean IO_FarRead (int handle, byte far *dest, long length);
-boolean IO_FarWrite (int handle, byte far *source, long length);
-boolean IO_ReadFile (char *filename, memptr *ptr);
-boolean IO_WriteFile(char *filename, void far *ptr, long length);
-long IO_LoadFile (char *filename, memptr *dst);
-void IO_CopyFile(char *sFilename, char *dFilename);
-void IO_CopyHandle(int sHandle, int dHandle, long num_bytes);
+
+void    IO_ReadFile (char *filename, void **dest);
+void    IO_WriteFile (char *filename, void *source, int32_t length);
+int32_t IO_LoadFile (char *filename, void **dest);
+void    IO_CopyHandle (FILE *sourcefile, FILE *destfile, int32_t numbytes);
 
 #endif
