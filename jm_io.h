18f7631b88815062cc80a02fa466f25dd8012f58
KillerSoftBP <emelya177@gmail.com>
2024-03-22 00:55:34 +0000

    Update jm_io.h

--- jm_io.h
+++ jm_io.h
@@ -7,6 +7,6 @@
 void    IO_ReadFile (char *filename, void **dest);
 void    IO_WriteFile (char *filename, void *source, int32_t length);
 int32_t IO_LoadFile (char *filename, void **dest);
-void    IO_CopyHandle (FILE *sourcefile, FILE *destfile, int32_t numbytes);
+void    IO_CopyFile (FILE *sourcefile, FILE *destfile, int32_t numbytes);
 
 #endif
