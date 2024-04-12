89a14f5ad4404ee037a6bc2013cc6255052711a2
KillerSoftBP <emelya177@gmail.com>
2024-04-12 21:21:20 +0100

    Fix informant scan checking out of map bounds
    It's amusing how DOS doesn't care one bit if you access memory out of bounds; it'll happily grab that address and manipulate it and even overwrite the data stored there, regardless of the consequences. Whereas the instant I did it here, boom! Segfault!
    
    I hope there aren't too many more of these buffer overruns waiting to happen...

--- 3d_agent.c
+++ 3d_agent.c
@@ -2856,6 +2856,12 @@ void Cmd_Use (void)
         {
             for (x = -MDIST; x < MDIST + 1; x++)
             {
+                if (!player->tilex + x
+                 || player->tilex + x >= mapwidth - 1
+                 || !player->tiley + y
+                 || player->tiley + y >= mapheight - 1)
+                    continue;
+
                 obj = actorat[player->tilex + x][player->tiley + y];
 
                 if (tilemap[player->tilex + x][player->tiley + y] || !ISPOINTER(obj))
